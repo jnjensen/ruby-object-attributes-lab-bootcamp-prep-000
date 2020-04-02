03c5393b406bc28db1ff7f61bcaa0fe8e636018f
James Jensen <jnjensen@live.com>
2020-04-02 02:08:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,11 @@
 class Dog 
+  def name = (dog_name)
+    @name = dog_name
+  end
   
+  def breed = (breed_name)
+    @breed = breed_name
+  end
   
   def name
     @name
@@ -8,3 +14,4 @@ class Dog
   def breed 
     @breed
   end
+end
